a48ebe90840cedd0c8b9114cbaff2a640bbebc59
LanLi2017 <lilan.scut@gmail.com>
2021-04-12 15:49:06 -0400

    bash file for running

--- run.bash
+++ run.bash
@@ -2,3 +2,8 @@ python main.py --project_id 2124203262743 --output usecase1/table_view/table_vie
 python main.py --project_id 2124203262743 --output usecase1/schema_view/schema_view.gv schema_view
 python main.py --project_id 2124203262743 --output usecase1/parallel_view/parallel_view.gv parallel_view
 python main.py --project_id 2124203262743 --output usecase1/modular_views/parallel_view modular_views
+
+python main.py --project_id 2494992270641 --output usecase2/table_view/table_view.gv table_view
+python main.py --project_id 2494992270641 --output usecase2/schema_view/schema_view.gv schema_view
+python main.py --project_id 2494992270641 --output usecase2/parallel_view/parallel_view.gv parallel_view
+python main.py --project_id 2494992270641 --output usecase2/modular_views/parallel_view modular_views
